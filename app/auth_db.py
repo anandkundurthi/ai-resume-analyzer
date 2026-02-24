bfed8cc7af8ebbf050589c7b98784ed708fcaf1e
Anand Sarma <anandsarma@Anands-MacBook-Air.local>
2026-02-24 09:56:29 +0530

    fix missing sqlalchemy imports

--- app/auth_db.py
+++ app/auth_db.py
@@ -1,7 +1,7 @@
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import hashlib
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
 DATABASE_URL = "sqlite:///./users.db"
 
 engine = create_engine(
