11133e789f82b0fa437d3bc75673f7e2ce5244e2
Anand Sarma <anandsarma@Anands-MacBook-Air.local>
2026-02-24 09:02:03 +0530

    fix database url syntax

--- app/auth_db.py
+++ app/auth_db.py
@@ -1,17 +1,16 @@
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
-from sqlalchemy.orm import declarative_base, sessionmaker, relationship
-from passlib.context import CryptContext
-from datetime import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "sqlite:///./app/users.db"
 
-# DATABASE
-DATABASE_URL = "DATABASE_URL = "sqlite:///./app/users.db""
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
-
 
 # PASSWORD HASHING
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
