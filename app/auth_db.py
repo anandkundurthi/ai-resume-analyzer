c7bc9f252209b51cd814f1521217fb4dc1a0e222
Anand Sarma <anandsarma@Anands-MacBook-Air.local>
2026-02-24 09:36:50 +0530

    fix sqlite path for render

--- app/auth_db.py
+++ app/auth_db.py
@@ -2,7 +2,7 @@ import hashlib
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./app/users.db"
+DATABASE_URL = "sqlite:///./users.db"
 
 engine = create_engine(
     DATABASE_URL,
